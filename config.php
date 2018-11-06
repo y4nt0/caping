e2dd3974ccaa41d7519993d000129dd797486bf0
Yanto0710 <44653110+Yanto0710@users.noreply.github.com>
2018-11-07 01:07:37 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
 
-$uid    ='xxxxxxxx'
-$n      ='xxxxxxxxxxxxxxxxxxxxxxx'
+$uid    ='61175837';
+$n      ='ffffffff8a733844ffffffff843626c2';
 #cari ui dan n pake paket capture.. dan jangan lupa pake kode refferal saya ya :v
